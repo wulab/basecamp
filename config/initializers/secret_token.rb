f8b4df06099045f6905cb3d7e77f590883be6397
Weera Wu <weera.or@wu.ac.th>
2011-01-20 13:02:31 +0700

    Initialize project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basecamp::Application.config.secret_token = '23006d78e0ad4188fb9b29795d2444f06e12339b4e691bde1f17d6fd50e7f5be91db574583ad618de544194f447c228b28738034c8bd229e3bf16387188bdcbd'
