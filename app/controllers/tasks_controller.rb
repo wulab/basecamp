f6f9430f25d36c2ded52a92895ca7f7a7af5a80e
Weera Wu <weera.or@wu.ac.th>
2011-01-21 12:22:44 +0700

    Fix various bugs found when run in production environment.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   def index
-    @tickets = Ticket.order("created_at desc")
+    @tickets = Ticket.order("created_at")
   end
 
+  # TODO: Migrate actions from tickets controller to here.
+  
 end
